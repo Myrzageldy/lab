115bcebceb01bf575fd44d0d369ee712e6f772bf
Азил Мырзагелдi <smitdzo6434@gmail.com>
2024-05-07 16:34:45 +0600

    Initial commit

---
+++ script1.js
@@ -0,0 +1,10 @@
+var number1 = parseInt(prompt("Введите первое число:"));
+var number2 = parseInt(prompt("Введите второе число:"));
+
+if (number1 > number2) {
+    alert("Первое число больше второго.");
+} else if (number1 < number2) {
+    alert("Первое число меньше второго.");
+} else {
+    alert("Числа равны.");
+}
---
+++ script2.js
@@ -0,0 +1,7 @@
+var inputString = prompt("Введите строку:");
+
+var topBorder = "*".repeat(inputString.length + 4);
+
+console.log(topBorder);
+console.log("* " + inputString + " *");
+console.log(topBorder);
